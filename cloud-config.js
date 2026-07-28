6c3ea1e7f1dee1d1be9d0c34cd08d2f71decf997
tk6871 <tk6871233@gmail.com>
2026-07-29 02:59:59 +0900

    Supabase 관리자 통계 연결 활성화

--- cloud-config.js
+++ cloud-config.js
@@ -1,6 +1,6 @@
 window.CBT_CLOUD_CONFIG = {
-  enabled: false,
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  enabled: true,
+  supabaseUrl: "https://bjwimcjatdcxjcuxikui.supabase.co",
+  supabaseAnonKey: "sb_publishable_yzKG2eMRjeL9ujVKoimIWQ_I33iqyVM",
   analyticsFunction: "cbt-analytics"
 };
